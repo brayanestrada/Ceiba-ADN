b3ebf2de45a14f0b6b6df49e75597c205adb0899
Brayan Estrada <brayan.estrada@ceiba.com.co>
2019-12-05 14:58:55 -0500

    First commit

---
+++ src/main/java/com/ventas/ventadepasajes/VentadepasajesApplication.java
@@ -0,0 +1,13 @@
+package com.ventas.ventadepasajes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentadepasajesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VentadepasajesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ventas/ventadepasajes/VentadepasajesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ventas.ventadepasajes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentadepasajesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
